59655a6b5fcf01fe7f130f3a8710fd72dcbbd583
Ezequiel Sosa <ezequieljsosa@gmail.com>
2021-08-26 11:49:58 -0300

    script to update pdb blast

--- scripts/update_genomes_pdb_blast.py
+++ scripts/update_genomes_pdb_blast.py
@@ -5,9 +5,8 @@ import os
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 
-os.environ["COMPOUND_TYPES_PATH"] = "/target/data/compound_type.csv"
+os.environ["COMPOUND_TYPES_PATH"] = os.getenv('COMPOUND_TYPES_PATH', "/target/data/compound_type.csv")
 from SNDG.BioMongo.Process.BioMongoDB import BioMongoDB
-
 
 if __name__ == "__main__":
     argv = sys.argv
@@ -17,19 +16,15 @@ if __name__ == "__main__":
                         help="set verbosity level [default: %(default)s]")
 
     parser.add_argument("-host", "--db_host", default='127.0.0.1')
-    parser.add_argument( "--port", default=27017)
+    parser.add_argument("--port", default=27017)
     parser.add_argument("-db", "--db_name", default='tdr')
-    parser.add_argument( "--pdbs_path", required=True)
-    parser.add_argument( "--organism_name", required=True)
-    parser.add_argument( "--remove_tmp", action='store_true')
-    parser.add_argument( "--cpu", default=4)
-    parser.add_argument( "--tmp_dir", default="./annotation/")
-
+    parser.add_argument("--pdbs_path", required=True)
+    parser.add_argument("--organism_name", required=True)
+    parser.add_argument("--remove_tmp", action='store_true')
+    parser.add_argument("--cpu", default=4)
+    parser.add_argument("--tmp_dir", default="./annotation/")
 
     args = parser.parse_args()
 
-    mdb = BioMongoDB(args.db_name,port=args.port, host=args.db_host)
-
-    mdb = BioMongoDB("tdr", port=args.port)
-
-    _common_annotations(args.organism_name, args.tmp_dir, args.cpu, args.remove_tmp, True, False,None,args.pdbs_path)
+    mdb = BioMongoDB(args.db_name, port=args.port, host=args.db_host)
+    _common_annotations(args.organism_name, args.tmp_dir, args.cpu, args.remove_tmp, True, False, None, args.pdbs_path)
