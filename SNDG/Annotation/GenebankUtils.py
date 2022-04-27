5409db9b4bb38883510d6ec7dcd3b52329064fc0
Ezequiel <ezequieljsosa@gmail.com>
2022-04-27 15:18:02 -0300

    -

--- SNDG/Annotation/GenebankUtils.py
+++ SNDG/Annotation/GenebankUtils.py
@@ -38,54 +38,55 @@ class GenebankUtils:
                             f.qualifiers["translation"] = [seq]
         bpio.write(contigs,hw_gb,"gb")
 
-    def proteins(self, h_or_str_gb, h_or_str_faa, otype="prot"):
-        if isinstance(h_or_str_gb, str):
-            h_gb = smart_parse(h_or_str_gb)
-        else:
-            h_gb = h_or_str_gb
+    # def proteins(self, h_or_str_gb, h_or_str_faa, otype="prot"):
+    #     if isinstance(h_or_str_gb, str):
+    #         h_gb = smart_parse(h_or_str_gb)
+    #     else:
+    #         h_gb = h_or_str_gb
+    #
+    #     if isinstance(h_or_str_faa, str):
+    #         h_faa = open(h_or_str_faa, "w")
+    #     else:
+    #         h_faa = h_or_str_faa
 
-        if isinstance(h_or_str_faa, str):
-            h_faa = open(h_or_str_faa, "w")
-        else:
-            h_faa = h_or_str_faa
+    def proteins(self, sequences, otype="prot"):
 
-        try:
-            for contig in h_gb:
-                org = (" [" + contig.annotations["organism"].replace("[", "_").replace("]", "_") + "]"
-                       ) if "organism" in contig.annotations else ""
-                for feature in contig.features:
-                    if feature.type in ["CDS", "RNA", "mat_peptide"]:
-                        seq = None
-                        description = feature.qualifiers["product"][0] if "product" else (
-                            feature.qualifiers["note"][0] if "note" in feature.qualifiers else "")
+        for contig in sequences:
+            org = (" [" + contig.annotations["organism"].replace("[", "_").replace("]", "_") + "]"
+                   ) if "organism" in contig.annotations else ""
+            for feature in contig.features:
+                if feature.type in ["CDS", "RNA", "mat_peptide"]:
+                    seq = None
+                    description = feature.qualifiers["product"][0] if "product" else (
+                        feature.qualifiers["note"][0] if "note" in feature.qualifiers else "")
 
-                        locus_tag = feature.qualifiers["locus_tag"][0]
-                        gene = feature.qualifiers["gene"][0] if "gene" in feature.qualifiers else ""
+                    locus_tag = feature.qualifiers["locus_tag"][0]
+                    gene = feature.qualifiers["gene"][0] if "gene" in feature.qualifiers else ""
 
-                        if feature.type == "mat_peptide":
-                            gene = gene + "_" + feature.qualifiers["product"][0]
-                            locus_tag = locus_tag + "_" + feature.qualifiers["product"][0].replace(" ", "_")
-                        if otype == "prot":
-                            if feature.type == "CDS" and "pseudo" not in feature.qualifiers:
-                                seq = Seq(feature.qualifiers["translation"][0])
-                            elif feature.type == "mat_peptide":
-                                seq = feature.extract(contig.seq).translate()
-                        else:
-                            seq = feature.extract(contig.seq)
-                        if seq:
-                            record = SeqRecord(id=locus_tag, name=gene, description=description + org, seq=seq)
-                            bpio.write(record, h_faa, "fasta")
-                        else:
-                            assert "pseudo" in feature.qualifiers
+                    if feature.type == "mat_peptide":
+                        gene = gene + "_" + feature.qualifiers["product"][0]
+                        locus_tag = locus_tag + "_" + feature.qualifiers["product"][0].replace(" ", "_")
+                    if otype == "prot":
+                        if feature.type == "CDS" and "pseudo" not in feature.qualifiers:
+                            seq = Seq(feature.qualifiers["translation"][0])
+                        elif feature.type == "mat_peptide":
+                            seq = feature.extract(contig.seq).translate()
+                    else:
+                        seq = feature.extract(contig.seq)
+                    if seq:
+                        record = SeqRecord(id=locus_tag, name=gene, description=description + org, seq=seq)
+                        yield record
+                    else:
+                        assert "pseudo" in feature.qualifiers
 
 
-        finally:
-            if isinstance(h_or_str_faa, str):
-                h_faa.close()
-            try:
-                h_gb.close()
-            except:
-                pass
+        # finally:
+        #     if isinstance(h_or_str_faa, str):
+        #         h_faa.close()
+        #     try:
+        #         h_gb.close()
+        #     except:
+        #         pass
 
 
 if __name__ == '__main__':
@@ -102,7 +103,7 @@ if __name__ == '__main__':
 
     cmd = subparsers.add_parser('genes', help='extracts the list of gene products from ')
     cmd.add_argument('input_bgk')
-    cmd.add_argument('output_dir', nargs='?', default=sys.stdout)
+    cmd.add_argument('output_fna', nargs='?', default=sys.stdout)
     cmd.add_argument('-otype', help="output type", choices=["prot", "nucl"], default="prot")
 
     args = parser.parse_args()
@@ -116,7 +117,24 @@ if __name__ == '__main__':
         if args.input_bgk != "-":
             gbk_h = smart_parse(args.input_bgk)
         else:
-            gbk_h = fileinput.input(args.input_bgk)
-        utils.proteins(gbk_h, args.output_dir,otype=args.otype)
+            gbk_h = bpio.parse(fileinput.input(args.input_bgk),"gb")
+
+        if isinstance(args.output_fna, str):
+            h_faa = open(args.output_fna, "w")
+        else:
+            h_faa = args.output_fna
+        try:
+            for seq in utils.proteins(gbk_h,otype=args.otype):
+                bpio.write(seq,h_faa,"fasta")
+        finally:
+            try:
+                h_faa.close()
+            except:
+                pass
+
+
+
+
+
     if args.command == "fix":
         utils.fixgb(fileinput.input(args.input_bgk), args.output_gb)
