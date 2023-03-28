fd14c491e71dae4ac0151494d4ddf1fc7c91cac8
Ezequiel <ezequieljsosa@gmail.com>
2023-03-28 17:44:54 -0300

    vc image

---
+++ images/ete3/render_tree.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+'''
+Created on Aug 24, 2015
+
+@author: eze
+'''
+import os
+
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+import argparse
+import pandas as pd
+
+
+
+from ete3 import Tree, TreeStyle, TextFace, PhyloTree
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tree", required=True, help="tree")
+    parser.add_argument("--image", required=True, help="output image")
+    parser.add_argument("--metadata", action=None, help="metadata csv. first column must be the node name")
+    parser.add_argument("--order", default=None, help="coma separated order of the metadata columns in the image")
+    parser.add_argument("--force_topology", action='store_true')
+
+    args = parser.parse_args()
+
+    assert os.path.exists(args.tree), "%s does not exists" % args.tree
+
+    if args.metadata:
+        assert os.path.exists(args.metadata)
+
+    tree = Tree(args.tree)
+
+    data = {}
+    order = []
+    metadata = None
+    if args.metadata:
+
+        metadata = pd.read_csv(args.metadata)
+        node_id = metadata.columns[0]
+        metadata = pd.read_csv(args.metadata, dtype={node_id: str})
+        metadata = metadata.fillna("")
+
+        for c in metadata.columns:
+            metadata[c] = metadata[c].astype(str)
+        for _, r in metadata.iterrows():
+            data[r[node_id].split(".variant")[0]] = {c: r[c] for c in metadata.columns}
+
+        metadata = data
+
+    # tu.render_tree(args.image, args.outgroup.split(",") if args.outgroup else None,
+    #               args.prune.split(",") if args.prune else None, metadata,
+    #               args.order.split(",") if args.order else None)
+
+    ts = TreeStyle()
+    if data:
+        if not args.order:
+            # order = list(data.keys())
+            first = data.keys()
+            first = list(first)[0]
+            order = data[first].keys()
+        else:
+            order = args.order
+        ts.show_leaf_name = True
+        ts.draw_guiding_lines = True
+        ts.force_topology = args.force_topology
+
+    for i, x in enumerate(order):
+        tf = TextFace(x)
+        tf.margin_left = 5
+        ts.aligned_header.add_face(tf, column=i)
+    if data:
+        for leaf in tree.get_leaves():
+            for i, col in enumerate(order):
+                tf = TextFace(data[leaf.name][col])
+                tf.margin_left = 5
+                leaf.add_face(tf, column=i, position="aligned")
+
+    tree.render(args.image, tree_style=ts)
+
+    #  render_tree.py --tree pepe.newick --image pepe1.png
--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-__version__ = "Undefined"
-for line in open('SNDG/__init__.py'):
-    if (line.startswith('__version__')):
-        exec(line.strip())
+__version__ = "2.0"
+
+#REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
     name='sndg-bio',
     packages=find_packages(),
     version=__version__,
-    scripts=["scripts/structurome.py", "scripts/vcf2aln.py", "scripts/render_tree.py"],
+    #scripts=["scripts/structurome.py", "scripts/vcf2aln.py", "scripts/render_tree.py"],
     description="My bioinformatic scripts",
     author='Ezequiel Sosa - SNDG',
-    install_requires=["tqdm", "bcbio-gff", "biopython", "goatools","networkx","python-libsbml","dna_features_viewer"],
+    install_requires=["tqdm", "bcbio-gff", "biopython", "goatools", "dna_features_viewer"],
     author_email='ezequieljsosa@gmail.com',
     url='https://github.com/ezequieljsosa/sndg-bio',
     download_url='https://github.com/ezequieljsosa/sndg-bio/archive/0.1.tar.gz',
     keywords=['bioinformatics', 'sequence', 'example'],
     classifiers=['Programming Language :: Python', 'Topic :: Scientific/Engineering :: Bio-Informatics',
                  'Intended Audience :: Science/Research', ],
+    extras_require={
+        'mongo': ['pymongo', 'mongoengine'],
+        'network': ['python-libsbml', "networkx"],
+        'phylo': ['ete3'],
+        'def': ["twine"]
+    },
 )
 
 # rm -r dist/;python setup.py sdist bdist_wheel;twine upload dist/*
