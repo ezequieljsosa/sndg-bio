318b2367ae408104c2c822fc5219fa2a14964931
Ezequiel Sosa <ezequieljsosa@gmail.com>
2023-04-04 15:05:07 -0300

    vc fix

--- scripts/VC.py
+++ scripts/VC.py
@@ -20,6 +20,8 @@ if __name__ == '__main__':
     cmd.add_argument('bam_folder')
     cmd.add_argument('vcfsraw_folder')
     cmd.add_argument('reference_file')
+    cmd.add_argument("--env", default=os.environ.get("GATK_ENV",""))
+
 
     #cmd.add_argument('--new_lt', default=None, help="new locus tag. Use ONLY if no locus tag is present")
 
@@ -27,11 +29,13 @@ if __name__ == '__main__':
     cmd.add_argument('vcfsraw_folder')
     cmd.add_argument('combined_raw_vcf')
     cmd.add_argument('reference_file')
+    cmd.add_argument("--env", default=os.environ.get("GATK_ENV",""))
 
     cmd = subparsers.add_parser('genotype_call', help='genotype call from raw vcf')
     cmd.add_argument('combined_raw_vcf')
     cmd.add_argument('combined_vcf')
     cmd.add_argument('reference_file')
+    cmd.add_argument("--env", default=os.environ.get("GATK_ENV",""))
 
     cmd = subparsers.add_parser('vcf_phylo', help='keeps only the variants useful for phylogeny and removes the rest')
     cmd.add_argument('combined_vcf')
@@ -53,8 +57,8 @@ if __name__ == '__main__':
         with tqdm(glob(f'{args.bam_folder}/*.bam')) as pbar:
             for bam in pbar:
                 pbar.set_description(bam)
-                vcf = os. bam.replace(".bam",".gvcf.gz")
-                cmd = f'''gatk  HaplotypeCaller -ERC GVCF -R "{args.reference_file}" \
+                vcf = bam.replace(".bam",".gvcf.gz")
+                cmd = f'''{args.env} gatk  HaplotypeCaller -ERC GVCF -R "{args.reference_file}" \
                           -ploidy 2 -I {bam} --output-mode EMIT_ALL_CONFIDENT_SITES \
                           -O "{args.vcfsraw_folder}/{vcf}" '''
                 sp.call(cmd,shell=True)
@@ -62,11 +66,11 @@ if __name__ == '__main__':
     if args.command == "combineraw":
         raw_vcfs = [f'"{x}"' for x in glob(f'{args.vcfsraw_folder}/*.gvcf.gz')]
         vcfs = " --variant".join(raw_vcfs)
-        cmd = f'''gatk CombineGVCFs -R "{args.reference_file}" -O "{args.combined_raw_vcf}" --variant {vcfs}'''
+        cmd = f'''{args.env} gatk CombineGVCFs -R "{args.reference_file}" -O "{args.combined_raw_vcf}" --variant {vcfs}'''
         sp.call(cmd,shell=True)
 
     if args.command == "genotype_call":
-        cmd = f'''gatk GenotypeGVCFs -R "{args.reference_file}" -ploidy 2 \
+        cmd = f'''{args.env} gatk GenotypeGVCFs -R "{args.reference_file}" -ploidy 2 \
                     -V "{args.combined_raw_vcf}" -O "{args.combined_vcf}"'''
         sp.call(cmd,shell=True)
 
